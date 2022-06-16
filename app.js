8f67c755335e040f1d022bceaf0a7003559dc38c
tomoda-kazuya <tomoda1207.work@gmail.com>
2022-06-16 18:21:37 +0900

    accesslog, applicationlogの追加作成

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const PORT = process.env.PORT;
 const path = require("path");
 const logger = require("./lib/log/logger.js");
+const applicationlogger = require("./lib/log/applicationlogger.js");
+const accesslogger = require("./lib/log/accesslogger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
 const app = express();
@@ -13,10 +15,16 @@ app.disable("x-powered-by");
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
+// Set access log
+app.use(accesslogger());
+
 // Dynamic resource rooting
 app.use("/", require("./routes/index.js"));
 
+// Set application log
+app.use(applicationlogger());
+
 // Execute web application 
 app.listen(PORT, () => {
-  logger.console.info(`Application listening at ${PORT}`);
+  logger.application.info(`Application listening at ${PORT}`);
 });
